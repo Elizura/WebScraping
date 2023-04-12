969d377e0f60d06f30065476e8baa25ccaea4364
Elizura <elisurfz@gmail.com>
2023-04-12 22:16:50 +0300

    the purpose of the app changed

--- intro.py
+++ intro.py
@@ -1,13 +1,42 @@
 from bs4 import BeautifulSoup
 import requests
+import requests.exceptions
+import urllib
+from collections import deque
+import re
+
+
 url = "https://realpython.com/python-web-scraping-practical-introduction/"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
-needed = []
-t = 0
-print(soup.get_text())
-for link in soup.find_all('a'):
-  needed.append(link.get('href'))
-  if t == 3: break
-  t += 1
-print(needed)
+urls = deque([url])
+scraped_urls = set()
+emails = set()
+count = 0
+
+try:
+  while urls:
+    count += 1
+    if count == 100: break
+    cur_url = urls.popleft()
+    scraped_urls.add(cur_url)
+    parts = urllib.parse.urlsplit(cur_url)
+    base_url = "{0: scheme}://{0:netloc}".format(parts)
+    path = url[url.rfind('./') + 1] if '/' in parts.path else url
+    try:
+      response = requests.get(url)
+    except(requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):      
+      continue
+    new_mail = set(re.findall(r"[a-z0-9\.\-+_] + @[a-z0-9\.\-+_] + \.[a-z]+", response, re.I))
+    emails.update(new_mail)
+    soup = BeautifulSoup(response.text, features="lxml")
+    for a in soup.find_all("a"):
+      link = a.attrs['href'] if 'href' in a.attrs else ''    
+      if link.startswith('/'):
+        link = base_url + link
+      elif not link.startswith('http'):
+        link = path + link
+      if not link in urls and not link in scraped_urls:
+        urls.append(link)
+except KeyboardInterrupt:
+  print('closing')
+for mail in emails:
+  print(mail)
